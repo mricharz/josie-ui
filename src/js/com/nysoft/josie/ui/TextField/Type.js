1742696b408555c727e8f0a09fb50ff907d39e34
mricharz_ext <manuel.richarz@holidaycheck.com>
2014-11-18 18:02:58 +0100

    Added more types in TextField.Type

--- src/js/com/nysoft/josie/ui/TextField/Type.js
+++ src/js/com/nysoft/josie/ui/TextField/Type.js
@@ -5,5 +5,16 @@ com.nysoft.josie.ui.TextField.Type = {
     Text: 'text',
     Email: 'email',
     Password: 'password',
+    Datetime: 'datetime',
+    DatetimeLocal: 'datetime-local',
+    Date: 'date',
+    Month: 'month',
+    Time: 'time',
+    Week: 'week',
+    Number: 'number',
+    Url: 'url',
+    Search: 'search',
+    Tel: 'tel',
+    Color: 'color',
     File: 'file'
 };
