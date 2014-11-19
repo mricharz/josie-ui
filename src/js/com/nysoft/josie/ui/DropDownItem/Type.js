7cf2c3b5b8a42cb5dfdddd24ed9851075c167013
mricharz_ext <manuel.richarz@holidaycheck.com>
2014-11-19 15:03:45 +0100

    Removed bullshit out of DropDownItem.Type.Default

--- src/js/com/nysoft/josie/ui/DropDownItem/Type.js
+++ src/js/com/nysoft/josie/ui/DropDownItem/Type.js
@@ -2,7 +2,7 @@ Josie.declare('com.nysoft.josie.ui.DropDownItem.Type');
 
 //Enum for DropDownItem-Type
 com.nysoft.josie.ui.DropDownItem.Type = {
-    Default: 'asd',
+    Default: '',
     Header: 'dropdown-header',
     Divider: 'divider'
 };
