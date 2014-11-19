b1b2d91bbdf3ef154efab14c00055f9298a71556
mricharz_ext <manuel.richarz@holidaycheck.com>
2014-11-19 10:32:42 +0100

    Extended TextField UI-Element

--- src/js/com/nysoft/josie/ui/TextField.js
+++ src/js/com/nysoft/josie/ui/TextField.js
@@ -4,6 +4,7 @@ Josie.require('com.nysoft.josie.ui.TextField.Type');
 com.nysoft.josie.core.Control.extend('com.nysoft.josie.ui.TextField', {
     meta: {
         type: { type: 'string', defaultValue: com.nysoft.josie.ui.TextField.Type.Text },
+        label: 'string',
         placeholder: 'string',
         disabled: { type: 'boolean', defaultValue: false },
         name: { type: 'string', defaultValue: '' },
@@ -12,13 +13,27 @@ com.nysoft.josie.core.Control.extend('com.nysoft.josie.ui.TextField', {
 
     _renderControl: function() {
         if(this.getDom()) {
-            var bDisabled = this.getDisabled(),
+            var sId = this.getId(),
+                sLabel = this.getLabel(),
+                bDisabled = this.getDisabled(),
                 sType = this.getType(),
                 sPlaceholder =  this.getPlaceholder(),
                 sName = this.getName(),
-                sContent = '<input';
+                sContent = '';
 
-            if(sType != com.nysoft.josie.ui.TextField.Type.File) {
+            if(sLabel) {
+                sContent += '<div class="form-group">';
+                sContent += '<label for="'+sId+'-input" class="col-sm-2 control-label">'+sLabel+'</label>';
+                sContent += '<div class="col-sm-10">';
+            }
+
+            sContent += '<input';
+
+            if(sLabel) {
+                sContent += ' id="'+sId+'-input"';
+            }
+
+            if(sType != com.nysoft.josie.ui.TextField.Type.File || sLabel) {
                 this.addCssClass('form-control');
             }
             sContent += this.writeCssClasses();
@@ -38,6 +53,11 @@ com.nysoft.josie.core.Control.extend('com.nysoft.josie.ui.TextField', {
 
             sContent += '/>';
 
+            if(sLabel) {
+                sContent += '</div>';
+                sContent += '</div>';
+            }
+
             this.replaceDom(sContent);
             this.getDom().click(jQuery.proxy(function(e) {
                 this.getClick().call(this, e);
@@ -56,6 +76,13 @@ com.nysoft.josie.core.Control.extend('com.nysoft.josie.ui.TextField', {
     setType: function(sType) {
         if(typeof sType == 'string' || sType == null) {
             this.setProperty('type', sType);
+            this._renderControl();
+        }
+    },
+
+    setLabel: function(sLabel) {
+        if(typeof sLabel == 'string' || sLabel == null) {
+            this.setProperty('label', sLabel);
             this._renderControl();
         }
     },
