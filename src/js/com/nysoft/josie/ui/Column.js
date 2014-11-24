dc6a128772d2b855b19e33e6bb2ffbf413b936c5
mricharz_ext <manuel.richarz@holidaycheck.com>
2014-11-24 08:39:14 +0100

    Added offset-property to Column

--- src/js/com/nysoft/josie/ui/Column.js
+++ src/js/com/nysoft/josie/ui/Column.js
@@ -1,15 +1,25 @@
 Josie.require('com.nysoft.josie.core.Control');
 Josie.require('com.nysoft.josie.ui.Column.Size');
+Josie.require('com.nysoft.josie.ui.Column.Offset');
 
 com.nysoft.josie.core.Control.extend('com.nysoft.josie.ui.Column', {
     meta: {
         size: { type: 'string', defaultValue: com.nysoft.josie.ui.Column.Size.Large[12] },
+        offset: { type: 'string', defaultValue: '' },
         content: 'object'
     },
 
     _renderControl: function() {
         if(this.getDom()) {
-            this.getDom().addClass(this.getSize());
+            var sSize = this.getSize(),
+                sOffset = this.getOffset();
+
+            if(sSize) {
+                this.getDom().addClass('col-' + sSize);
+            }
+            if(sOffset) {
+                this.getDom().addClass('col-' + sOffset);
+            }
         }
         this._super('_renderControl', arguments);
     }
---
+++ src/js/com/nysoft/josie/ui/Column/Offset.js
@@ -0,0 +1,61 @@
+Josie.declare('com.nysoft.josie.ui.Column.Offset');
+
+//Enum for Button-Size
+com.nysoft.josie.ui.Column.Offset = {
+    Large: {
+        1: 'lg-offset-1',
+        2: 'lg-offset-2',
+        3: 'lg-offset-3',
+        4: 'lg-offset-4',
+        5: 'lg-offset-5',
+        6: 'lg-offset-6',
+        7: 'lg-offset-7',
+        8: 'lg-offset-8',
+        9: 'lg-offset-9',
+        10: 'lg-offset-10',
+        11: 'lg-offset-11',
+        12: 'lg-offset-12'
+    },
+    Medium: {
+        1: 'md-offset-1',
+        2: 'md-offset-2',
+        3: 'md-offset-3',
+        4: 'md-offset-4',
+        5: 'md-offset-5',
+        6: 'md-offset-6',
+        7: 'md-offset-7',
+        8: 'md-offset-8',
+        9: 'md-offset-9',
+        10: 'md-offset-10',
+        11: 'md-offset-11',
+        12: 'md-offset-12'
+    },
+    Small: {
+        1: 'sm-offset-1',
+        2: 'sm-offset-2',
+        3: 'sm-offset-3',
+        4: 'sm-offset-4',
+        5: 'sm-offset-5',
+        6: 'sm-offset-6',
+        7: 'sm-offset-7',
+        8: 'sm-offset-8',
+        9: 'sm-offset-9',
+        10: 'sm-offset-10',
+        11: 'sm-offset-11',
+        12: 'sm-offset-12'
+    },
+    ExtraSmall: {
+        1: 'xs-offset-1',
+        2: 'xs-offset-2',
+        3: 'xs-offset-3',
+        4: 'xs-offset-4',
+        5: 'xs-offset-5',
+        6: 'xs-offset-6',
+        7: 'xs-offset-7',
+        8: 'xs-offset-8',
+        9: 'xs-offset-9',
+        10: 'xs-offset-10',
+        11: 'xs-offset-11',
+        12: 'xs-offset-12'
+    }
+};
