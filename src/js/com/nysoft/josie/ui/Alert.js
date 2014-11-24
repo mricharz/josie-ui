b9d738d1d8f9241f99a5cc4023399928eea70815
mricharz_ext <manuel.richarz@holidaycheck.com>
2014-11-24 11:35:31 +0100

    Made Alerts dismissible

--- src/js/com/nysoft/josie/ui/Alert.js
+++ src/js/com/nysoft/josie/ui/Alert.js
@@ -4,21 +4,29 @@ Josie.require('com.nysoft.josie.ui.Alert.Type');
 com.nysoft.josie.core.Control.extend('com.nysoft.josie.ui.Alert', {
     meta: {
         text: 'string',
+        dismissible: { type: 'boolean', defaultValue: false },
         type: { type: 'string', defaultValue: com.nysoft.josie.ui.Alert.Type.Info }
     },
 
     _renderControl: function() {
         if(this.getDom()) {
-            var sType = this.getType(),
+            var bDismissible = this.getDismissible(),
+                sType = this.getType(),
                 sContent = '<div';
 
             this.addCssClass('alert');
+            if(bDismissible) {
+                this.addCssClass('alert-dismissible');
+            }
             if(sType) {
                 this.addCssClass('alert-'+sType);
             }
             sContent += this.writeCssClasses();
 
             sContent += '>';
+            if(bDismissible) {
+                sContent += '<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>';
+            }
             sContent += '</div>';
 
             this.replaceDom(sContent);
